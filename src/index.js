d46a1eaf180a42be1c9f423b420c908b206e1dbd
Kethan <kchandrakanth@thapovan-inc.com>
2021-07-31 00:08:26 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { whenAdded } from "when-elements";
 import { hooked, hasEffect, dropEffect } from "uhooks";
 
-export default when = (selector, callback) => {
+export default function (selector, callback) {
 	whenAdded(selector, (element) => {
 		const kids = [...element.childNodes];
 		const props = {};
