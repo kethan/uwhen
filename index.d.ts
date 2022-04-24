565288de32d3b0fce73c5fee8053a9d5255a240c
Kethan Chandrakanth <kethanc@tissrv.com>
2022-04-24 12:31:37 +0530

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 type LifeCycleCallback = {
-    connected: () => {};
-    disconnected: () => {};
-    attributeChanged: (name: string, oldValue: string, newValue: string) => void;
-    observedAttributes: string[];
+    connected?: Function;
+    disconnected?: Function;
+    attributeChanged?: (name: string, oldValue: string, newValue: string) => void;
+    observedAttributes?: string[];
 }
 type Callback = ((el: HTMLElement) => LifeCycleCallback);
 declare function when(selector: string, callback: Callback): void;
